340fd1a58f4b72d69ad41257149c8281d4ce1423
Osman Ungur <osmanungur@me.com>
2024-05-05 00:59:08 +0200

    Add new configs to install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,11 @@ set -o xtrace
 
 cd ~
 ln -s dotfiles/bash_profile .bash_profile
+ln -s dotfiles/zshrc .zshrc
 ln -s dotfiles/inputrc .inputrc
 ln -s dotfiles/screenrc .screenrc
 ln -s dotfiles/vimrc .vimrc
+ln -s dotfiles/tmux.conf .tmux.conf
+ln -s dotfiles/gitconfig .gitconfig
 touch dotfiles/bash_profile.post
 
